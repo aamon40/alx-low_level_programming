6d908d530f0fd6113d5c89855d594d471724b894
Nyore <aamonthedon@gmail.com>
2022-09-22 13:16:02 +0100

    splash

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 
 /**
  * leet - a function that encodes a string into 1337.
- * @s: an input string to encode
+ * @s: the input string to encode
  * Return: An encode string
  */
 char *leet(char *s)
