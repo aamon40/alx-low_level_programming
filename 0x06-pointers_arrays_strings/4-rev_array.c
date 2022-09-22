48498dd0dbbc234dcec8dea7fb104957e47470de
Nyore <aamonthedon@gmail.com>
2022-09-22 13:46:48 +0100

    corrected

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i = 0;
-    int aux;
+	int i = 0;
+	int aux;
 
-    while (i < n--)
-    {
-        aux = a[i];
-        a[i++] = a[n];
-        a[n] = aux;
-    }
+	while (i < n--)
+	{
+		aux = a[i];
+		a[i++] = a[n];
+		a[n] = aux;
+	}
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,25 @@
 int check_seperators(char c);
 
 /**
- * cap_string - a function which capitalizes all words of a string.
+ * cap_string - a function that capitalizes all words of a string.
  * @s: An input string to capitalize letters
  * Return: pointer to s
  */
 char *cap_string(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-            s[i] -= 32;
+	while (s[i])
+	{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
 
-        if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-            s[i + 1] -= 32;
-        i++;
-    }
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
 
 /**
@@ -30,13 +30,13 @@ char *cap_string(char *s)
  */
 int check_seperators(char c)
 {
-    int i = 0;
-    char seperators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
-                           '"', '(', ')', '{', '}'};
-    for (; i < 13; i++)
-    {
-        if (c == seperators[i])
-            return (1);
-    }
-    return (0);
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+	'"', '(', ')', '{', '}' };
+	fo (; i < 13; i++	r)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
 }
