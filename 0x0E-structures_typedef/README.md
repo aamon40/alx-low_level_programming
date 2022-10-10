c1b4cd09e6d90d090e265351fb05351f6a749f9f
Nyore <aamonthedon@gmail.com>
2022-10-10 23:32:56 +0100

    initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This contains tasks for learning structures in C.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+voidprin t_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
